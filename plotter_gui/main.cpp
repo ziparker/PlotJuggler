0490182a0cdad4a9edc33095dc037324040d2d30
Eurecat Robotic Lab <davide.faconti@gmail.com>
2017-01-20 15:18:14 +0100

    splashscreen!!!

--- plotter_gui/main.cpp
+++ plotter_gui/main.cpp
@@ -1,19 +1,67 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QSplashScreen>
+#include <QThread>
+
+
+QString getFunnySubtitle(){
+
+  qsrand(time(NULL));
+  int n = qrand() % 18;
+  switch(n)
+  {
+  case 0: return "The best excuse to buy a second monitor";
+  case 1: return "Now with 100% more splashscreens";
+  case 2: return "Because command line tools sucks";
+  case 3: return "Data at your fingertips";
+  case 4: return "You might have used Kst2";
+  case 5: return "Have you starred me on Github?";
+  case 6: return "Insert [useless splashscreen] here";
+  case 7: return "Data, data everywhere";
+  case 8: return "Plots at the speed of mouse";
+  case 9: return "Just Plot It!";
+  case 10: return "More than you can plot";
+  }
+  return "Juggle with data";
+}
 
 int main(int argc, char *argv[])
 {
-    QApplication app(argc, argv);
-    QCoreApplication::setApplicationName("PlotJuggler");
-    QCoreApplication::setApplicationVersion("0.7.0");
+  QApplication app(argc, argv);
+  QCoreApplication::setApplicationName("PlotJuggler");
+  QCoreApplication::setApplicationVersion("0.7.0");
 
-    qApp->setStyleSheet(QString("QToolTip {\n"
-                                "   border: 1px solid black;\n"
-                                "   border-radius: 6px;\n"
-                                "   background: white;\n}" ));
+  qApp->setStyleSheet(QString("QToolTip {\n"
+                              "   border: 1px solid black;\n"
+                              "   border-radius: 6px;\n"
+                              "   background: white;\n}" ));
 
-    MainWindow w;
-    w.show();
+  QPixmap main_pixmap(":/splash/resources/splash.jpg");
 
-    return app.exec();
+  QPainter painter;
+  painter.begin( &main_pixmap);
+  painter.setPen(QColor(50, 50, 50));
+
+  QString subtitle = getFunnySubtitle();
+  painter.setFont( QFont("Arial", 30) );
+
+  painter.drawText( QRect(30, 200, 600, 100), Qt::AlignHCenter | Qt::AlignVCenter, subtitle );
+  painter.end();
+
+  QSplashScreen splash(main_pixmap);
+  splash.show();
+
+
+  for (int i =0; i<(20 + subtitle.size()/2) && !splash.isHidden(); i++ ) {
+    app.processEvents();
+    QThread::msleep(100);
+    splash.raise();
+  }
+
+ splash.close();
+
+  MainWindow w;
+  w.show();
+
+  return app.exec();
 }
