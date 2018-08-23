9a9f45009e6921b018f91ef9dedf95d93befa285
Davide Faconti <davide.faconti@eurecat.org>
2018-08-23 15:00:38 +0200

    cleanups

--- plugins/ROS/DataLoadROS/dataload_ros.cpp
+++ plugins/ROS/DataLoadROS/dataload_ros.cpp
@@ -34,6 +34,64 @@ size_t getAvailableRAM()
     return info.freeram;
 }
 
+std::vector<std::pair<QString,QString>> DataLoadROS::getAndRegisterAllTopics()
+{
+  std::vector<std::pair<QString,QString>> all_topics;
+  rosbag::View bag_view ( *_bag, ros::TIME_MIN, ros::TIME_MAX, true );
+  std::vector<const rosbag::ConnectionInfo*> connections = bag_view.getConnections();
+
+  for(unsigned i=0; i<connections.size(); i++)
+  {
+      const auto&  topic      =  connections[i]->topic;
+      const auto&  md5sum     =  connections[i]->md5sum;
+      const auto&  datatype   =  connections[i]->datatype;
+      const auto&  definition =  connections[i]->msg_def;
+
+      all_topics.push_back( std::make_pair(QString( topic.c_str()), QString( datatype.c_str()) ) );
+      _parser->registerMessageDefinition(topic, RosIntrospection::ROSType(datatype), definition);
+      RosIntrospectionFactory::registerMessage(topic, md5sum, datatype, definition);
+  }
+}
+
+void DataLoadROS::storeMessageInstancesAsUserDefined(PlotDataMapRef& plot_map,
+                                                     const std::string& prefix,
+                                                     bool use_header_stamp)
+{
+  using namespace RosIntrospection;
+
+  rosbag::View bag_view ( *_bag, ros::TIME_MIN, ros::TIME_MAX, true );
+
+  RenamedValues renamed_value;
+
+  for(rosbag::MessageInstance msg_instance: bag_view )
+  {
+      const std::string& topic_name  = msg_instance.getTopic();
+      const std::string key = prefix + topic_name;
+      double msg_time = msg_instance.getTime().toSec();
+
+      if(use_header_stamp)
+      {
+          const auto header_stamp = FlatContainerContainHeaderStamp(renamed_value);
+          if(header_stamp)
+          {
+              const double time = header_stamp.value();
+              if( time > 0 ) {
+                msg_time = time;
+              }
+          }
+      }
+
+      auto plot_pair = plot_map.user_defined.find( key );
+
+      if( plot_pair == plot_map.user_defined.end() )
+      {
+          plot_pair = plot_map.addUserDefined( key );
+      }
+      PlotDataAny& plot_raw = plot_pair->second;
+      plot_raw.pushBack( PlotDataAny::Point(msg_time, nonstd::any(std::move(msg_instance)) ));
+  }
+}
+
 PlotDataMapRef DataLoadROS::readDataFromFile(const QString &file_name, bool use_previous_configuration)
 {
     if( _bag ) _bag->close();
@@ -42,9 +100,6 @@ PlotDataMapRef DataLoadROS::readDataFromFile(const QString &file_name, bool use_
     _parser.reset( new RosIntrospection::Parser );
 
     using namespace RosIntrospection;
-
-    std::vector<std::pair<QString,QString>> all_topics;
-    PlotDataMapRef plot_map;
 
     try{
         _bag->open( file_name.toStdString(), rosbag::bagmode::Read );
@@ -56,25 +111,8 @@ PlotDataMapRef DataLoadROS::readDataFromFile(const QString &file_name, bool use_
                              QString(ex.what()) );
         return PlotDataMapRef();
     }
-    QElapsedTimer timer;
-    timer.start();
 
-    rosbag::View bag_view ( *_bag, ros::TIME_MIN, ros::TIME_MAX, true );
-    std::vector<const rosbag::ConnectionInfo*> connections = bag_view.getConnections();
-
-    for(unsigned i=0; i<connections.size(); i++)
-    {
-        const auto&  topic      =  connections[i]->topic;
-        const auto&  md5sum     =  connections[i]->md5sum;
-        const auto&  datatype   =  connections[i]->datatype;
-        const auto&  definition =  connections[i]->msg_def;
-
-        all_topics.push_back( std::make_pair(QString( topic.c_str()), QString( datatype.c_str()) ) );
-        _parser->registerMessageDefinition(topic, ROSType(datatype), definition);
-        RosIntrospectionFactory::registerMessage(topic, md5sum, datatype, definition);
-    }
-
-    int count = 0;
+    auto all_topics = getAndRegisterAllTopics();
 
     //----------------------------------
     QSettings settings( "IcarusTechnology", "PlotJuggler");
@@ -98,33 +136,29 @@ PlotDataMapRef DataLoadROS::readDataFromFile(const QString &file_name, bool use_
         if( dialog->exec() == static_cast<int>(QDialog::Accepted) )
         {
             _default_topic_names = dialog->getSelectedItems();
-
-            // load the rules
-            if( dialog->checkBoxUseRenamingRules()->isChecked())
-            {
-                _rules = RuleEditing::getRenamingRules();
-                _use_renaming_rules = true;
-            }
-            else{
-                _rules.clear();
-                _use_renaming_rules = false;
-            }
+            settings.setValue("DataLoadROS/default_topics", _default_topic_names);
+            settings.setValue("DataLoadROS/use_renaming", _use_renaming_rules);
         }
         else{
             return PlotDataMapRef();
         }
-        settings.setValue("DataLoadROS/default_topics", _default_topic_names);
-        settings.setValue("DataLoadROS/use_renaming", _use_renaming_rules);
     }
+
+    _use_renaming_rules = dialog->checkBoxUseRenamingRules()->isChecked();
 
     if( _use_renaming_rules )
     {
+      _rules = RuleEditing::getRenamingRules();
       for(const auto& it: _rules) {
         _parser->registerRenamingRules( ROSType(it.first) , it.second );
       }
     }
-    const int max_array_size = dialog->maxArraySize();
-    const std::string prefix = dialog->prefix().toStdString();
+    else{
+      _rules.clear();
+    }
+    const int max_array_size    = dialog->maxArraySize();
+    const std::string prefix    = dialog->prefix().toStdString();
+    const bool use_header_stamp = dialog->checkBoxUseHeaderStamp()->isChecked();
 
     //-----------------------------------
     std::set<std::string> topic_selected;
@@ -132,8 +166,6 @@ PlotDataMapRef DataLoadROS::readDataFromFile(const QString &file_name, bool use_
     {
         topic_selected.insert( topic.toStdString() );
     }
-
-    const bool use_header_stamp = dialog->checkBoxUseHeaderStamp()->isChecked();
 
     QProgressDialog progress_dialog;
     progress_dialog.setLabelText("Loading... please wait");
@@ -147,17 +179,22 @@ PlotDataMapRef DataLoadROS::readDataFromFile(const QString &file_name, bool use_
     progress_dialog.setRange(0, bag_view_selected.size()-1);
     progress_dialog.show();
 
+    PlotDataMapRef plot_map;
     FlatMessage flat_container;
     std::vector<uint8_t> buffer;
-    RenamedValues renamed_value;
-    
+    RenamedValues renamed_values;
+
     bool parsed = true;
     bool warning_use_header_stamp_ignored = false;
     bool warning_monotonic_time = true;
-    bool Warning_numerical_cancellation = false;
+    bool warning_numerical_cancellation = false;
     std::unordered_set<std::string> problematic_headerstamp;
     std::unordered_set<std::string> problematic_monotonic;
     std::unordered_set<std::string> problematic_cancellation;
+    int msg_count = 0;
+
+    QElapsedTimer timer;
+    timer.start();
 
     for(rosbag::MessageInstance msg_instance: bag_view_selected )
     {
@@ -166,9 +203,9 @@ PlotDataMapRef DataLoadROS::readDataFromFile(const QString &file_name, bool use_
 
         buffer.resize(msg_size);
 
-        if( count++ %100 == 0)
+        if( msg_count++ %100 == 0)
         {
-            progress_dialog.setValue( count );
+            progress_dialog.setValue( msg_count );
             QApplication::processEvents();
 
             if( progress_dialog.wasCanceled() ) {
@@ -180,13 +217,13 @@ PlotDataMapRef DataLoadROS::readDataFromFile(const QString &file_name, bool use_
         msg_instance.write(stream);
 
         parsed &= _parser->deserializeIntoFlatContainer( topic_name, absl::Span<uint8_t>(buffer), &flat_container, max_array_size );
-        _parser->applyNameTransform( topic_name, flat_container, &renamed_value );
+        _parser->applyNameTransform( topic_name, flat_container, &renamed_values );
 
         double msg_time = msg_instance.getTime().toSec();
 
         if(use_header_stamp)
         {
-            const auto header_stamp = FlatContainedContainHeaderStamp(renamed_value);
+            const auto header_stamp = FlatContainerContainHeaderStamp(renamed_values);
             if(header_stamp)
             {
                 const double time = header_stamp.value();
@@ -195,12 +232,12 @@ PlotDataMapRef DataLoadROS::readDataFromFile(const QString &file_name, bool use_
                 }
                 else{
                   warning_use_header_stamp_ignored = true;
-                  problematic_cancellation.insert(topic_name);
+                  problematic_headerstamp.insert(topic_name);
                 }
             }
         }
 
-        for(const auto& it: renamed_value )
+        for(const auto& it: renamed_values )
         {
             const std::string key = prefix + it.first;
             const RosIntrospection::Variant& value = it.second;
@@ -231,7 +268,7 @@ PlotDataMapRef DataLoadROS::readDataFromFile(const QString &file_name, bool use_
                 bool error = (val_i != static_cast<uint64_t>(val_d));
                 if(error)
                 {
-                    Warning_numerical_cancellation = true;
+                    warning_numerical_cancellation = true;
                     problematic_cancellation.insert(key);
                 }
                 plot_data.pushBack( PlotData::Point(msg_time, val_d) );
@@ -243,7 +280,7 @@ PlotDataMapRef DataLoadROS::readDataFromFile(const QString &file_name, bool use_
                 bool error = (val_i != static_cast<int64_t>(val_d));
                 if(error)
                 {
-                    Warning_numerical_cancellation = true;
+                    warning_numerical_cancellation = true;
                     problematic_cancellation.insert(key);
                 }
                 plot_data.pushBack( PlotData::Point(msg_time, val_d) );
@@ -255,36 +292,9 @@ PlotDataMapRef DataLoadROS::readDataFromFile(const QString &file_name, bool use_
         } //end of for renamed_value
     }
 
-    for(rosbag::MessageInstance msg_instance: bag_view )
-    {
-        const std::string& topic_name  = msg_instance.getTopic();
-        const std::string key = prefix + topic_name;
-        double msg_time = msg_instance.getTime().toSec();
-
-        if(use_header_stamp)
-        {
-            const auto header_stamp = FlatContainedContainHeaderStamp(renamed_value);
-            if(header_stamp)
-            {
-                const double time = header_stamp.value();
-                if( time > 0 ) {
-                  msg_time = time;
-                }
-            }
-        }
-
-        auto plot_pair = plot_map.user_defined.find( key );
-
-        if( plot_pair == plot_map.user_defined.end() )
-        {
-            plot_pair = plot_map.addUserDefined( key );
-        }
-        PlotDataAny& plot_raw = plot_pair->second;
-        plot_raw.pushBack( PlotDataAny::Point(msg_time, nonstd::any(std::move(msg_instance)) ));
-    }
+    storeMessageInstancesAsUserDefined(plot_map, prefix, use_header_stamp);
 
     qDebug() << "The loading operation took" << timer.elapsed() << "milliseconds";
-
 
     if( !parsed )
     {
@@ -317,7 +327,7 @@ PlotDataMapRef DataLoadROS::readDataFromFile(const QString &file_name, bool use_
         dialog->exec();
     }
 
-    if( Warning_numerical_cancellation )
+    if( warning_numerical_cancellation )
     {
         QString message = "During the parsing process, one or more conversions to double failed"
                           " because of numerical cancellation.\n"
