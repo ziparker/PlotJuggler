4716b51aa1bb74f94c4485c380e6b924fb47bd1c
Davide Faconti <davide.faconti@gmail.com>
2019-09-13 15:18:10 +0200

    new color palette

--- include/PlotJuggler/random_color.h
+++ include/PlotJuggler/random_color.h
@@ -7,17 +7,20 @@ inline QColor randomColorHint()
 {
     static int index = 0;
     QColor color;
-    switch( index%9 )
+   // https://matplotlib.org/3.1.1/users/dflt_style_changes.html
+    switch( index%10 )
     {
-    case 0:  color =  QColor("#1464a0");   break; //blue
-    case 1:  color =  QColor("#05740d");   break; //green
-    case 2:  color =  QColor("#ff1318");   break; //red
-    case 3:  color =  QColor("#f700f7");   break; //magenta
-    case 4:  color =  QColor("#0033ee");   break; //dark blue
-    case 5:  color =  QColor("#007777");   break; //dark cyan
-    case 6:  color =  QColor("#50b47f");   break; // green-ish
-    case 7:  color =  QColor("#f4531d");   break; //orange
-    case 8:  color =  QColor("#8e3488");   break; //volet
+    case 0:  color =  QColor("#1950ff");   break;
+    case 1:  color =  QColor("#ff7c00");   break;
+    case 2:  color =  QColor("#1ac938");   break;
+    case 3:  color =  QColor("#e81721");   break;
+    case 4:  color =  QColor("#9139e2");   break;
+
+    case 5:  color =  QColor("#00d7ff");   break;
+    case 6:  color =  QColor("#f14cc1");   break;
+    case 7:  color =  QColor("#ffc400");   break;
+    case 8:  color =  QColor("#9f4800");   break;
+    case 9:  color =  QColor("#a3a3a3");   break;
     }
     index++;
     return color;
