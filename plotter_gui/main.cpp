9b32c6dd19a890c1c05177c225e979326d801297
Davide Faconti <davide.faconti@gmail.com>
2019-10-11 13:35:11 +0200

    Add one more

--- plotter_gui/main.cpp
+++ plotter_gui/main.cpp
@@ -14,8 +14,8 @@ QPixmap getFunnySplashscreen(){
     qsrand(time(nullptr));
 
     auto getNum = [](){
-        const int last_image_num = 43;
-        int n = qrand() % (last_image_num+3);
+        const int last_image_num = 44;
+        int n = qrand() % (last_image_num+2);
         if ( n > last_image_num ){ n = 0; }
         return n;
     };
