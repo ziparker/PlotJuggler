ff31d3b4194a65ebc58f629ecec9cbfbef688cf8
Davide Faconti <davide.faconti@gmail.com>
2020-05-10 10:53:39 +0200

    segfault fixed in TypeHasHeader

--- plugins/ROS/ros2_introspection/src/ros2_introspection.cpp
+++ plugins/ROS/ros2_introspection/src/ros2_introspection.cpp
@@ -38,6 +38,10 @@ bool TypeHasHeader(const rosidl_message_type_support_t* type_support)
     if( members->member_count_>=1 && members->members_)
     {
         const MessageMember& first_field = members->members_[0];
+        if( first_field.members_ == nullptr )
+        {
+            return false;
+        }
         const auto* header_members = static_cast<const MessageMembers*>(first_field.members_->data);
         if( strcmp( header_members->message_name_, "Header") == 0 &&
             strcmp( header_members->message_namespace_, "std_msgs::msg") == 0)
