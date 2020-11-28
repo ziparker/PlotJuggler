3a48d45ccf1c22e8601065269acc1f2c7b760131
Davide Faconti <davide.faconti@gmail.com>
2020-11-28 12:30:11 +0100

    minor updates

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@
 #include <QNetworkAccessManager>
 #include <QNetworkReply>
 #include <QJsonDocument>
+#include <QDir>
 
 #include "PlotJuggler/transform_function.h"
 #include "transforms/first_derivative.h"
@@ -117,7 +118,13 @@ int main(int argc, char* argv[])
 
   try {
 #ifdef COMPILED_WITH_CATKIN
-    extra_path = QCoreApplication::applicationDirPath() + "_ros";
+    //TODO: use pluginlib instead
+    QDir ros_plugins_dir( QCoreApplication::applicationDirPath() + "_ros" );
+    if( !ros_plugins_dir.exists() || ros_plugins_dir.isEmpty() )
+    {
+      throw std::runtime_error("Missing ros plugins directory");
+    }
+    extra_path = ros_plugins_dir.path();
 #endif
 #ifdef COMPILED_WITH_AMENT
     extra_path = QString::fromStdString(ament_index_cpp::get_package_prefix("plotjuggler_ros"));
